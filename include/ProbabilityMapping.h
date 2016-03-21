592b74e58453de93e82cd2fb4de613693930e098
thibugio <rmf61@case.edu>
2016-03-21 11:42:29 -0400

    pulling

--- include/ProbabilityMapping.h
+++ include/ProbabilityMapping.h
@@ -50,23 +50,6 @@ class Mat;
 
 class ProbabilityMapping {
 public:
-<<<<<<< HEAD
-//        struct depthHo;
-	//struct depthHo {
-//		float depth;
-//		float sigma;
-//	};
-
-        ProbabilityMapping();
-	/* * \brief void first_loop(ORB_SLAM::KeyFrame kf, std::vector<std::vector<depthHo> >, std::vector<depthHo>*): return results of epipolar search (depth hypotheses) */
-	void FirstLoop(ORB_SLAM::KeyFrame *kf, std::vector<std::vector<depthHo> >& ho);
-        /* * \brief void stereo_search_constraints(): return min, max inverse depth */
-        void StereoSearchConstraints(ORB_SLAM::KeyFrame* kf, float* min_depth, float* max_depth);
-	/* * \brief void epipolar_search(): return distribution of inverse depths/sigmas for each pixel */
-        void EpipolarSearch(ORB_SLAM::KeyFrame *kf1, ORB_SLAM::KeyFrame *kf2, int x, int y, cv::Mat gradx, cv::Mat grady, cv::Mat grad, float min_depth, float max_depth, depthHo* dh);
-	/* * \brief void inverse_depth_hypothesis_fusion(const vector<depthHo> H, depthHo dist): 
-=======
-
 	struct depthHo {
 		float depth;
 		float sigma;
@@ -82,10 +65,11 @@ public:
 	/* * \brief void epipolar_search(): return distribution of inverse depths/sigmas for each pixel */
         void EpipolarSearch(ORB_SLAM::KeyFrame *kf1, ORB_SLAM::KeyFrame *kf2, int x, int y, cv::Mat gradx, cv::Mat grady, cv::Mat grad, float min_depth, float max_depth, depthHo &dh);
 	/* * \brief void inverse_depth_hypothesis_fusion(const vector<depthHo> H, depthHo* dist): 
->>>>>>> 0412eb271ac61504be761a5e33e70f2010ce807a
 	 * *         get the parameters of depth hypothesis distrubution from list of depth hypotheses */
         void InverseDepthHypothesisFusion(const std::vector<depthHo>& h, depthHo* dist);
-	/* * \brief void intraKeyFrameDepthChecking(std::vector<std::vector<depthHo> > h, int imrows, int imcols): intra-keyframe depth-checking, smoothing, and growing. */
+        /* * \brief void
+         * intraKeyFrameDepthChecking(std::vector<std::vector<depthHo> > h, int
+         * imrows, int imcols): intra-keyframe depth-checking, smoothing, and growing */
         void IntraKeyFrameDepthChecking(std::vector<std::vector<depthHo> >& ho, int imrows, int imcols);
 	/* * \brief void interKeyFrameDepthChecking(ORB_SLAM::KeyFrame* currentKF, std::vector<std::vector<depthHo> > h, int imrows, int imcols): 
          * *         inter-keyframe depth-checking, smoothing, and growing. */
@@ -100,7 +84,6 @@ private:
         void GetImageGradient(const cv::Mat& image, cv::Mat* gradx, cv::Mat* grady, cv::Mat* grad);
         void GetGradientOrientation(int x, int y, const cv::Mat& gradx, const cv::Mat& grady, float* th);
         void GetInPlaneRotation(ORB_SLAM::KeyFrame* k1, ORB_SLAM::KeyFrame* k2, float* th);
-        void GetIntensityGradient(cv::Mat im, float* g);
         void PixelNeighborSupport(std::vector<std::vector<depthHo> > H, int x, int y, std::vector<depthHo>& support);
         void PixelNeighborNeighborSupport(std::vector<std::vector<depthHo> > H, int px, int py, std::vector<std::vector<depthHo> >& support);
         void GetIntensityGradient_D(const cv::Mat& ImGrad, float a, float b, float c, int px, float* q);
